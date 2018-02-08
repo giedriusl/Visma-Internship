bc411b00b4a0ad8781793f6f8ac8f7a5583ec414
Giedrius Lukočius <giedrius.lukocius@visma.com>
2018-02-08 16:54:16 +0200

    Add console app with httpclient

--- AnagramSolver/MainApp/Program.cs
+++ AnagramSolver/MainApp/Program.cs
@@ -1,8 +1,10 @@
 ﻿using DBReader;
 using Implementation;
 using System;
+using System.Collections.Generic;
 using System.Configuration;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
 namespace MainApp
@@ -10,7 +12,7 @@ namespace MainApp
     public class Program
     {
         static HttpClient client = new HttpClient();
-
+        private DisplayConsole _display = new DisplayConsole();
         static void Main(string[] args)
         {
             RunAsync().GetAwaiter().GetResult();
@@ -18,7 +20,36 @@ namespace MainApp
 
         static async Task RunAsync()
         {
-            client.BaseAddress = new Uri("http://localhost:54566/");
+            DisplayConsole _display = new DisplayConsole();
+            //client.BaseAddress = new Uri("http://localhost:54566/");
+            //client.DefaultRequestHeaders.Accept.Clear();
+            //client.DefaultRequestHeaders.Accept.Add(
+            //    new MediaTypeWithQualityHeaderValue("application/json"));
+            try
+            {
+                var input = "alus";
+                var anagrams = await GetAsync($"http://localhost:54566/Home/GetAnagramsFromDictionary?input={input}");
+                Console.WriteLine(anagrams);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
         }
+
+        static async Task<string> GetAsync(string path)
+        {
+            string product = null;
+            HttpResponseMessage response = await client.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                product = await response.Content.ReadAsStringAsync();
+
+            }
+            return product;
+        }
+
+        
     }
 }
