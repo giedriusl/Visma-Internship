5c145ac3e6447a3de234769c30e11ffdee0ca771
Giedrius Lukocius <giedrius.lukocius@gmail.com>
2018-02-20 17:20:41 +0200

    Delegates, events

---
+++ AnagramSolver/MainApp/DisplayWithEvents.cs
@@ -0,0 +1,24 @@
+﻿using Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Interfaces.Delegates;
+
+namespace MainApp
+{
+
+    public class DisplayWithEvents
+    {
+        public event PrintHandler OutputDisplay;
+        public delegate void PrintHandler(object sender, DisplayEventArgs e);
+        public EventArgs e = null;
+
+        public void OnOutputDisplay(DisplayEventArgs e)
+        {
+            OutputDisplay(this, e);
+        }
+
+    }
+}
